807f5e9d19a976d2d79ae542fe4610f457ef8665
WakFerdi <pangerankoplak28@gmail.com>
2021-12-30 14:20:54 +0700

    Latihan 5.2: Melakukan Operasi Sederhana dengan operator aritmatika

--- learn.js
+++ learn.js
@@ -8,7 +8,20 @@ console.log(myNumber);
 console.log(myString);
 console.log(myBoolean);
 console.log(phi);
-//menampilkan informasi dalam bentu alert pada window
+//menampilkan informasi dalam bentuk alert pada window
 window.alert("Angka favorit saya adalah :"+ myNumber);
 window.alert('Angka favorit saya adalah :' +myNumber);
 window.alert(`Angka favorit saya adalah : ${myNumber}`);
+//membuat operasi sederhana dengan operator aritmatika
+    let x= 8
+    let y= 4
+    console.log('8, 4');
+    console.log('8 + 4 =', x + y);
+    console.log('8 - 4 =', x - y);
+    console.log('8 * 4 =', x * y);
+    console.log('8 / 4 =', x / y);
+    console.log('8 % 4 =', x % y);
+    console.log('8++ =', x++);
+    console.log('++8 =', ++x);
+    console.log('8-- =', x--);
+    console.log('--8 =', --x);
