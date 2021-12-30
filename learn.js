d30ffad9ea453e113f0b1a6fac6309d53d3f1aed
WakFerdi <pangerankoplak28@gmail.com>
2021-12-30 13:58:28 +0700

    Latihan 5.1: membuat variabel dan menampilkannya

---
+++ learn.js
@@ -0,0 +1,14 @@
+//membuat variabel
+let myNumber= 16;
+let myString= "Hello"
+let myBoolean= true
+const phi= 3.14
+//menampilkan informasi ke console
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(phi);
+//menampilkan informasi dalam bentu alert pada window
+window.alert("Angka favorit saya adalah :"+ myNumber);
+window.alert('Angka favorit saya adalah :' +myNumber);
+window.alert(`Angka favorit saya adalah : ${myNumber}`);
