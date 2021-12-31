5253b69aa2bef0c4b5db1139f9ab5576de775d42
WakFerdi <pangerankoplak28@gmail.com>
2021-12-31 17:09:28 +0700

    Latihan 5.6: Bermain dengan array

--- learn.js
+++ learn.js
@@ -3,15 +3,18 @@ let myNumber= 16;
 let myString= "Hello"
 let myBoolean= true
 const phi= 3.14
+
 //menampilkan informasi ke console
 console.log(myNumber);
 console.log(myString);
 console.log(myBoolean);
 console.log(phi);
+
 //menampilkan informasi dalam bentuk alert pada window
 window.alert("Angka favorit saya adalah :"+ myNumber);
 window.alert('Angka favorit saya adalah :' +myNumber);
 window.alert(`Angka favorit saya adalah : ${myNumber}`);
+
 //membuat operasi sederhana dengan operator aritmatika
     let x= 8
     let y= 4
@@ -25,7 +28,8 @@ window.alert(`Angka favorit saya adalah : ${myNumber}`);
     console.log('++x =', ++x);
     console.log('x-- =', x--);
     console.log('--x =', --x);
-     //membuat operasi sederhana dengan operator perbandingan
+
+//membuat operasi sederhana dengan operator perbandingan
      let a= 8
      let b= 4
      console.log(a, b);
@@ -37,7 +41,8 @@ window.alert(`Angka favorit saya adalah : ${myNumber}`);
      console.log('a != b =', a != b);
      console.log('a === b =', a === b);
      console.log('a !== b =', a !== b);
-     //membuat operasi sederhana dengan operator logika
+
+//membuat operasi sederhana dengan operator logika
      console.log('true && true =', true && true);
      console.log('true && false =', true && false);
      console.log('false && true =', false && true);
@@ -48,7 +53,8 @@ window.alert(`Angka favorit saya adalah : ${myNumber}`);
      console.log('false || false =', false || false);
      console.log('!true =', !true);
      console.log('!false =', !false);
-     //meghitung fungsi luas persegi, luas segitiga, dan volume balok
+
+//meghitung fungsi luas persegi, luas segitiga, dan volume balok
      function luas_persegi(s){ //menggunakan 1 parameter
          let luas = s*s; //rumus luas persegi
          return luas; //mengembalikan nilai ke yang memanggil 
@@ -66,3 +72,31 @@ window.alert(`Angka favorit saya adalah : ${myNumber}`);
          return volume; //mengembalikan nilai ke yang memanggil
      }
      volume_balok(10, 5, 2);
+
+//membuat array
+     var products = ["Keyboard", "Monitor", "Mouse", "Flashdisk"]; //elemen-elemen array
+     console.log(products[0]); //menampilkan array di console
+     console.log(products[1]); //menampilkan array di console
+     console.log(products[2]); //menampilkan array di console
+     console.log(products[3]); //menampilkan array di console
+     
+     products.push("Hardisk"); //menambahkan item baru di array
+     console.log(products); //menampilkan array di console
+
+     products.pop(products); //menghapus remove isi array paling akhir
+     console.log(products);
+
+     products.length; //menghitung ukuran array
+     console.log(products);
+
+     products.unshift("Hardisk"); //memasukkan item ke array tetapi disimpan di paling awal
+     console.log(products);
+
+     products.shift(products); //menghilangkan item pertama di array
+     console.log(products);
+
+     products.sort(); //mengurutkan array
+     console.log(products);
+
+     products.reverse(); //membalikkan urutan array
+     console.log(products);
